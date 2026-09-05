f066c3de8452412cb0aa554b17cc8ccf594f1fa4
DebashisBPG <debashisborpatragohain@gmail.com>
2026-09-05 22:44:48 +0530

    Add files via upload

---
+++ 20260905-224016.c
@@ -0,0 +1,15 @@
+/*
+*program: To add two given numbers
+*/
+
+
+#include<stdio.h>
+
+int main()
+{
+    int A,B;
+    printf("Insert two integers:");
+    scanf("%d %d",&A,&B);
+    printf("%d+%d=%d",A,B,A+B);
+    return 0;
+}
