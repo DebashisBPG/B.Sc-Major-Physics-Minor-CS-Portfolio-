a7c2a8c5fb864fbaabab896fda2ee6ef0b73281c
DebashisBPG <debashisborpatragohain@gmail.com>
2026-09-05 23:55:12 +0530

    Implement basic arithmetic operations in program5.c
    Fix syntax errors and improve formatting in the arithmetic operations.

---
+++ program5.c
@@ -0,0 +1,15 @@
+/*
+*program: add,sub,multi,div two given no.
+*/
+#include<stdio.h>
+int main()
+{
+  int A,B;
+  printf("Insert two numbers:");
+  scanf("%d %d",&A.&B);
+  printf("%d+%d=%d",A,B,A+B);
+  printf("%d-%d=%d",A,B,A-B);
+  printf("%d×%d=%d",A,B,A*B);
+  printf("%d÷%d=%d",A,B,A/B);
+  return 0;
+}  
