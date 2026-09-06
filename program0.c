419c4318c8a691dfc76143a840464a5399867d82
DebashisBPG <debashisborpatragohain@gmail.com>
2026-09-06 20:35:15 +0530

    Add program to calculate sum of N numbers
    This program prompts the user to input N numbers and calculates their sum.

---
+++ program0.c
@@ -0,0 +1,19 @@
+/*
+*program:print the sum of total N inserted numbers
+*/
+#include<stdio.h>
+int main()
+{
+  int N,no,i=1,sum=0;
+  printf("How many numbers you want to add:");
+  scanf("%d",&N);
+  while(i<=N)
+  {
+    printf("Insert the %d number:",i);
+    scanf("%d",&no);
+    sum=sum+no;
+    i++;
+  }
+  printf("sum=%d",sum);
+  return 0;
+}
